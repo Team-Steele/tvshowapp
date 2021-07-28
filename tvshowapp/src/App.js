c47593eddf62c2232f1d45f8ac966d2e837b7396
Andoner <Andsmith316@gmail.com>
2021-07-27 19:00:33 -0700

    3rd try

--- tvshowapp/src/App.js
+++ tvshowapp/src/App.js
@@ -1,42 +1,39 @@
-
-import React from 'react';
-import { useState, useEffect } from 'react';
-import Users from './components/Users';
-import tvShowList from './components/tvShowList';
-
+import React from "react";
+import { useState, useEffect } from "react";
+import Users from "./components/Users";
+import tvShowList from "./components/tvShowList";
 
 function App() {
   const [users, setUsers] = useState([]);
 
   const [shows, setShows] = useState([]);
-  
+
   const [searchString, setSearchString] = useState("simpsons");
 
-  const [lastSearch, setLastSearch] = useState('');
-  
-  const searchOptions = {
-    api: 'https://api.tvmaze.com/search/shows',
+  const [lastSearch, setLastSearch] = useState("");
 
+  const searchOptions = {
+    api: "https://api.tvmaze.com/search/shows",
   };
   function getShows(searchString) {
     /* Build a URL from the searchOptions object */
     const url = `${searchOptions.api}?q=${searchString}`;
-    
+
     fetch(url)
-    .then(response => response.json())
-    .then(response => {
-      setShows(response.data);
-      // Set the lastSearch to the searchString value
-      setLastSearch(searchString);
-      // Set the searchString in state to an empty string
-      setSearchString('');
-    })
-    .catch(console.error);
+      .then((response) => response.json())
+      .then((response) => {
+        setShows(response.data);
+        // Set the lastSearch to the searchString value
+        setLastSearch(searchString);
+        // Set the searchString in state to an empty string
+        setSearchString("");
+      })
+      .catch(console.error);
   }
   // function handleChange(event) {
   //   setSearchString(event.target.value);
   // }
-  
+
   // function handleSubmit(event) {
   //   event.preventDefault();
   //   getShows(searchString);
@@ -51,7 +48,7 @@ function App() {
       <h1>Tv Show Review App</h1>
       <Users user={users} />
       <tvShowList shows={shows} />
-  
+
       {/* <SearchForm
         handleChange={handleChange}
         handleSubmit={handleSubmit}
@@ -62,6 +59,3 @@ function App() {
 }
 
 export default App;
-
-
-
